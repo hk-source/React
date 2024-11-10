394fa19fa405396bec8b5c777d95b2d7777456b0
Hasan Kamran <71834822+hk-source@users.noreply.github.com>
2024-11-10 18:59:14 +0530

    React Component

--- App.js
+++ App.js
@@ -1,4 +1,26 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
-console.log(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1",{id:"heading"},"React Learning");
+
+// React Element
+const jsxHeading = <h1 id="heading">React Learning using JSX</h1>
+
+// React Functional Component
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        React using JSX 🚀
+    </h1>
+);
+
+//Component Composition - One component into another
+const HeadingComponent = () => (
+    <div>
+        <Title />
+        <h1 className="heading">React Functional Component</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeadingComponent/>);
